9132dfb4ef8dd5f6d144dcbbe6134f762d4637c4
snmatsui <snmatsui@users.noreply.github.com>
2023-06-05 21:20:50 +0900

    feat: winget

---
+++ private_winget.ps1
@@ -0,0 +1,43 @@
+# utility
+winget install Google.Chrome
+winget install Microsoft.PowerToys
+winget install File-New-Project.EarTrumpet
+# StrokesPlus
+
+# - editor
+# clibor
+winget install Kuro.Mery
+
+# - finder
+winget install wiztree
+# Files
+
+# - media
+winget install VideoLAN.VLC
+winget install Learnpulse.Screenpresso
+# Explzh
+# Splash
+# VBCable
+
+# communication
+winget install LINE.LINE
+winget install SlackTechnologies.Slack
+winget install OBSProject.OBSStudio
+winget install Discord.Discord
+winget install Zoom.Zoom
+
+# program
+# fork
+# winget install vscode
+winget install Docker.DockerDesktop
+
+# benchmark
+winget install CrystalDewWorld.CrystalDiskInfo
+winget install CrystalDewWorld.CrystalDiskMark
+winget install cinebench
+winget install cpu-z
+winget install passmark
+
+# gaming
+# winget install steam
+winget install "Epic Games Store"
